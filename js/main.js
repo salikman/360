4c1dac5292066ea45fe70be3e98bd17664c8943e
Max <savonikmaksim@gmail.com>
2020-10-08 11:39:36 +0300

    v1.1

--- js/main.js
+++ js/main.js
@@ -10,19 +10,9 @@ class Demo {
 			sizer: element.querySelector('.my-sizer-element'),
 		});
 
-		// Log events.
-		// this.addShuffleEventListeners();
 		this._activeFilters = [];
 		this.addFilterButtons();
 	}
-	// addShuffleEventListeners() {
-	// 	this.shuffle.on(Shuffle.EventType.LAYOUT, (data) => {
-	// 		console.log('layout. data:', data);
-	// 	});
-	// 	this.shuffle.on(Shuffle.EventType.REMOVED, (data) => {
-	// 		console.log('removed. data:', data);
-	// 	});
-	// }
 
 	addFilterButtons() {
 		const options = document.querySelector('.filter__btn');
@@ -72,6 +62,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 (function($) {
+	var $logo = $('.navbar__logo img');
+		$logo.data('initial', $logo.attr('src'));
+
+	$(window).on('scroll load resize', function () {
+		var wScroll = $(this).scrollTop();
+		var wWidth = $(this).width();
+
+		wWidth <= 1024 ? $('#nav').addClass('fixed-nav') : $('#nav').removeClass('fixed-nav');
+		
+		if ($('body').hasClass('not-is-home')) {
+			wWidth <= 1024 ? $logo.attr('src', $logo.data('initial')) : $logo.attr('src', $logo.data('inverse'));
+		}
+	});
+	///////////////////////////
+	// Smooth scroll
+	$("#nav .menu li a[href^='#']").on('click', function(e) {
+		e.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 600);
+	});
     ///////////////////////////
 	// Btn nav collapse
 	$('#nav .navbar__collapse').on('click', function() {
@@ -80,6 +92,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	///////////////////////////
 	// sliders
+	$('#production-filming').slick({
+		slidesToShow: 1,
+        swipeToSlide: true,
+        arrows: false,
+		autoplay: true,
+		dots: true,
+		dotsClass: 'dots dots-production',
+		autoplaySpeed: 2300,
+		infinite: false,
+        speed: 1000
+	})
     $('#gallery-slider').slick({
         slidesToShow: 5,
         swipeToSlide: true,
@@ -100,7 +123,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }]
     });
-	$('#reviews').slick({
+	$('#reviews-slider').slick({
 		infinite: true,
 		slidesToShow: 4,
 		slidesToScroll: 1,
@@ -108,7 +131,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		prevArrow: '<button type="button" class="arrow arrow-prev"><svg viewBox="0 0 18 10" id="icon_arrow" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 1.01c0-.24-.08-.48-.23-.67-.33-.41-.9-.45-1.27-.08L8.99 7.68 1.49.26C1.12-.11.55-.07.22.34c-.33.42-.3 1.05.07 1.42L8.4 9.77c.34.34.85.34 1.19 0l8.1-8.01c.2-.2.31-.48.31-.75z"></path></svg></button>',
 		nextArrow: '<button type="button" class="arrow arrow-next"><svg viewBox="0 0 18 10" id="icon_arrow" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 1.01c0-.24-.08-.48-.23-.67-.33-.41-.9-.45-1.27-.08L8.99 7.68 1.49.26C1.12-.11.55-.07.22.34c-.33.42-.3 1.05.07 1.42L8.4 9.77c.34.34.85.34 1.19 0l8.1-8.01c.2-.2.31-.48.31-.75z"></path></svg></button>',
 		dots: true,
-		dotsClass: 'dots',
+		dotsClass: 'dots dots-reviews',
 		responsive: [
 			{
 				breakpoint: 1366,
